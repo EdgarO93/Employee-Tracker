d9ce4211524f7dce2ceb8e441c13ad056e509441
Edgar Ortega <edort93@gmail.com>
2021-10-16 23:35:12 -0500

    added sequelize sync and authenticate method

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Include packages needed for this application
 const inquirer = require("inquirer");
+
+// Import the connection object
+const sequelize = require('./db/connection');
+
+
+//test that was able to connect
+sequelize.sync().then(() => {
+   sequelize.authenticate();console.log('Connection has been established successfully.');
+  });
+
