6950d55052c692a84ac9d6442a9b1a3b3a57bb51
Edgar Ortega <edort93@gmail.com>
2021-10-16 23:12:17 -0500

    added classes code to modify later

---
+++ db/index.js
@@ -0,0 +1,42 @@
+//will have to create classes here
+
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return "Employee";
+    }
+}
+
+class Manager extends Employee {
+    constructor(name,id,email,officeNumber){
+        super(name,id,email)
+        this.officeNumber = officeNumber;
+    }
+    getOfficeNumber(){
+        return this.officeNumber
+
+    };
+    getRole() {
+        return "Manager"
+    }
+}
+
+module.exports=  Manager;
+
+
+
+
+module.exports = Employee;
