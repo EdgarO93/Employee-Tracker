52eba55aa98e43d01584dd0920d92d7608c1a33e
Edgar Ortega <edort93@gmail.com>
2021-10-16 22:47:10 -0500

    added create table

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXISTS employees_DB;
 CREATE DATABASE employees_DB;
 
 USE employees_DB;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
