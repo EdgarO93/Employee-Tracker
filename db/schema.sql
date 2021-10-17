85249536098d52aa476d68a9b9cecff91f5e419a
Edgar Ortega <edort93@gmail.com>
2021-10-16 22:51:08 -0500

    added create table for department and role

--- db/schema.sql
+++ db/schema.sql
@@ -6,9 +6,23 @@ USE employees_DB;
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(45) NULL,
-  last_name VARCHAR(45) NULL,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NULL,
   PRIMARY KEY (id)
 );
