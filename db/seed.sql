7a78e3d5fe11ba2dea87399a21d2fa1381ff9458
Edgar Ortega <edort93@gmail.com>
2021-10-17 12:33:01 -0500

    updated tables

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,8 @@
 INSERT INTO department (name)
-VALUES ("test");
+VALUES("Engineering"), ("Sales"), ("Finance");
        
+INSERT INTO role(title, salary, department_id)
+VALUES('Engineer', 85000, 1), ("Senior Engineer", 125000, 1);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Testy', 'Test', 1, 2), ('Justin', 'Fields', 1, null);
