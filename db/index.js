2092068bd4df4c99c02710a21a9df9cb1c8884c2
Edgar Ortega <edort93@gmail.com>
2021-10-17 17:08:14 -0500

    commented out code, addded functions that will be exported

--- db/index.js
+++ db/index.js
@@ -1,42 +1,50 @@
-//will have to create classes here
+// //will have to create classes here
 
-class Employee {
-    constructor (name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-    getName() {
-        return this.name;
-    }
-    getId() {
-        return this.id;
-    }
-    getEmail() {
-        return this.email;
-    }
-    getRole() {
-        return "Employee";
-    }
-}
+// class Employee {
+//     constructor (name, id, email) {
+//         this.name = name;
+//         this.id = id;
+//         this.email = email;
+//     }
+//     getName() {
+//         return this.name;
+//     }
+//     getId() {
+//         return this.id;
+//     }
+//     getEmail() {
+//         return this.email;
+//     }
+//     getRole() {
+//         return "Employee";
+//     }
+// }
 
-class Manager extends Employee {
-    constructor(name,id,email,officeNumber){
-        super(name,id,email)
-        this.officeNumber = officeNumber;
-    }
-    getOfficeNumber(){
-        return this.officeNumber
+// class Manager extends Employee {
+//     constructor(name,id,email,officeNumber){
+//         super(name,id,email)
+//         this.officeNumber = officeNumber;
+//     }
+//     getOfficeNumber(){
+//         return this.officeNumber
 
-    };
-    getRole() {
-        return "Manager"
-    }
-}
+//     };
+//     getRole() {
+//         return "Manager"
+//     }
+// }
 
-module.exports=  Manager;
-
-
+// module.exports=  Manager;
 
 
-module.exports = Employee;
+
+
+// module.exports = Employee;
+
+const db = require('./db/connection');
+
+
+module.exports = {
+    addNewEmployee,addNewRole,addNewDept,removeEmployee,removeDept,
+    updateEmployeeRole,viewRoles,viewDepartments
+ }
