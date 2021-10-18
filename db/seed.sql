c8d43b4edc3e99e3fc6f1629ed0c44e05b14ada6
Edgar Ortega <edort93@gmail.com>
2021-10-17 23:11:56 -0500

    added more entries

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (department_name)
-VALUES("Engineering"), ("Sales"), ("Finance");
+VALUES("Engineering"), ("Sales"), ("Finance"), ("Human Resources");
        
 INSERT INTO role(title, salary, department_id)
-VALUES('Engineer', 85000, 1), ("Senior Engineer", 125000, 1),('Sales', 65000, 2), ("Sales Lead", 95000, 2);
+VALUES('Engineer', 85000, 1), ("Senior Engineer", 125000, 1),('Sales', 65000, 2), ("Sales Lead", 95000, 2),("Accountant", 70000,3), ("HR Lead", 70000,4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('Testy', 'Test', 1, 2), ('Justin', 'Fields', 1, null);
+VALUES ('Rusty', 'Testy', 1, 3), ('Andy','Dalton', 1, 3), ('Justin', 'Fields', 2, null), ('John', 'Smith', 4, null),('Joe','Greene',6, null);
