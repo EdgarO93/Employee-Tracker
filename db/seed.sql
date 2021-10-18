26e37d559a3ed5698fdba4a2ee8a9667e9c31635
Edgar Ortega <edort93@gmail.com>
2021-10-17 20:12:20 -0500

    updated the roles table

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO department (name)
 VALUES("Engineering"), ("Sales"), ("Finance");
        
 INSERT INTO role(title, salary, department_id)
-VALUES('Engineer', 85000, 1), ("Senior Engineer", 125000, 1);
+VALUES('Engineer', 85000, 1), ("Senior Engineer", 125000, 1),('Sales', 65000, 2), ("Sales Lead", 95000, 2);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ('Testy', 'Test', 1, 2), ('Justin', 'Fields', 1, null);
