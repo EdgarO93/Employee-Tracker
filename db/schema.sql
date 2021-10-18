26855d339bd5cb15f2330fd4438bbff770bda94a
Edgar Ortega <edort93@gmail.com>
2021-10-17 21:05:20 -0500

    updated department name

--- db/schema.sql
+++ db/schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE employee (
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
